c675e746d5c502ae81ce6225c6786c5c09639250
antoniotorone <antoniotorone8913@gmail.com>
2021-09-27 20:03:22 +0000

    profile app created

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -27,14 +30,12 @@ class Order(models.Model):
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
-#update the total order si a function that will need in signals.py 
     def update_total(self):
         """
         Update grand total each time a line item is added,
@@ -59,7 +60,6 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
-
 
 
 class OrderLineItem(models.Model):
