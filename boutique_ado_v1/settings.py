146a8dcbd396e49c9dc053a6704aacb7c239a662
antoniotorone <antoniotorone8913@gmail.com>
2021-10-04 17:46:55 +0000

    heroku app created and connected

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.exists("env.py"):
     import env
@@ -125,11 +126,13 @@ WSGI_APPLICATION = 'boutique_ado_v1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+ the dj database url comes from heroku in settings after you created the new app
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
