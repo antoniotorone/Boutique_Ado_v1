03aa38f0d6952ce87f68602170f4136ec777eab2
antoniotorone <antoniotorone8913@gmail.com>
2021-10-05 17:45:10 +0000

    deployment part 2

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-q36^5a^xxarxu$h-8!8cr&f*cs)trn8!e-##cjyi(!m-xgd@h8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anto8913-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,9 +125,13 @@ WSGI_APPLICATION = 'boutique_ado_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
- the dj database url comes from heroku in settings after you created the new app
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
