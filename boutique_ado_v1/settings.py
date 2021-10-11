ccd243ede17a52f3f5c6be2b0f768786b0b5592e
antoniotorone <antoniotorone8913@gmail.com>
2021-10-11 19:31:14 +0000

    re deploy test

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -116,7 +116,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'boutique_ado_v1.wsgi.application'
 
 
 # Database
@@ -221,5 +221,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
